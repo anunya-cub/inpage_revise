cb828b4c1a210dc8ae9b8c72070dd6fb5a6b62f2
anunya-cub <anunya.foythong@gmail.com>
2021-06-26 22:43:26 +0700

    edit slide

--- js/javascript.js
+++ js/javascript.js
@@ -1,16 +1,21 @@
-$('.accordion-toggle').click(function(e) {
-    e.preventDefault();  
-  $("a:first-of-type").removeClass('chev');
-  
+$(".accordion-toggle").click(function (e) {
+  e.preventDefault();
+  $("a:first-of-type").removeClass("chev");
+
   var $this = $(this);
-  if ($this.next().hasClass('show')) {
-      $this.next().removeClass('show');
-      $this.next().slideUp(350);
+  if ($this.next().hasClass("show")) {
+    $this.next().removeClass("show");
+    $this.next().slideUp(350);
   } else {
-      $this.parent().parent().find('li .accordion-inner').removeClass('show');
-      $this.parent().parent().find('li .accordion-inner').slideUp(350);
-      $this.toggleClass('chev');
-      $this.next().toggleClass('show');
-      $this.next().slideToggle(350);
+    $this.parent().parent().find("li .accordion-inner").removeClass("show");
+    $this.parent().parent().find("li .accordion-inner").slideUp(350);
+    $this.toggleClass("chev");
+    $this.next().toggleClass("show");
+    $this.next().slideToggle(350);
   }
 });
+
+
+function changeSlide(data){
+  
+}
